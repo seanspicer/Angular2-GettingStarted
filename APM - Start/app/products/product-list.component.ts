4b953dd45098d7905a17a6e2d1fcf98a67183254
Sean Spicer <s.spicer@sigmacubed.com>
2016-06-03 15:23:59 -0500

    Complete Course

--- APM - Start/app/products/product-list.component.ts
+++ APM - Start/app/products/product-list.component.ts
@@ -1,52 +1,43 @@
-import { Component, OnInit } from 'angular2/core';
-import { ProductFilterPipe } from './product-filter.pipe';
+import { Component, OnInit }  from 'angular2/core';
+import { ROUTER_DIRECTIVES } from 'angular2/router';
+
 import { IProduct } from './product';
+import { ProductFilterPipe } from './product-filter.pipe';
+import { StarComponent } from '../shared/star.component';
+import { ProductService } from './product.service';
 
 @Component({
-    selector: 'pm-products',
     templateUrl: 'app/products/product-list.component.html',
     styleUrls: ['app/products/product-list.component.css'],
-    pipes: [ProductFilterPipe]
+    pipes: [ProductFilterPipe],
+    directives: [StarComponent, ROUTER_DIRECTIVES]
 })
-
 export class ProductListComponent implements OnInit {
-    
-    // Properties
-    pageTitle: string = "Product List";
+    pageTitle: string = 'Product List';
     imageWidth: number = 50;
     imageMargin: number = 2;
     showImage: boolean = false;
-    listFilter: string = 'cart';
-    
-    products: IProduct[] = [
-        {
-            "productId": 1,
-            "productName": "Leaf Rake",
-            "productCode": "GDN-0011",
-            "releaseDate": "March 19, 2016",
-            "description": "Leaf rake with 48-inch wooden handle.",
-            "price": 19.95,
-            "starRating": 3.2,
-            "imageUrl": "http://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
-        },
-        
-        {
-            "productId": 2,
-            "productName": "Garden Cart",
-            "productCode": "GDN-0023",
-            "releaseDate": "March 18, 2016",
-            "description": "15 gallon capacity rolling garden cart",
-            "price": 32.99,
-            "starRating": 4.2,
-            "imageUrl": "http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
-        }
-    ];
-    
-    toggleImage():void  {
+    listFilter: string = '';
+    errorMessage: string;
+    products: IProduct[];
+
+
+    constructor(private _productService: ProductService) {
+
+    }
+
+    toggleImage(): void {
         this.showImage = !this.showImage;
     }
-    
-    ngOnInit() : void {
-        console.log('In OnInit');
+
+    ngOnInit(): void {
+           this._productService.getProducts()
+                     .subscribe(
+                       products => this.products = products,
+                       error =>  this.errorMessage = <any>error);
+    }
+
+    onRatingClicked(message: string): void {
+        this.pageTitle = 'Product List: ' + message;
     }
 }
